881edf218e61b5e5f2dee641d64a50e698a20a64
kranjitha <ranjitha.korrapati@gmail.com>
2019-06-22 03:20:56 -0400

    Changed the package name

--- R/Create_bet.R
+++ R/Create_bet.R
@@ -1,6 +1,6 @@
 
 Create_bet <- function(dd){
-	path <- paste(system.file(package="dogs"), "bet.py", sep="/")
+	path <- paste(system.file(package="artml"), "bet.py", sep="/")
 	source_python(path)
 	a = create_bet(dd)
 	return(a)
